e983bf0be5d83c6cbb815a6fc5b93df7acb87265
+ <+>
2023-09-14 12:41:44 +0300

    completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(arr):
+    if len(arr) == 0:
+        return ""
+    elif len(arr) == 1:
+        return arr[0]
+    elif len(arr) == 2:
+        return f"{arr[0]} and {arr[1]}"
+    else:
+        elements_except_last = ", ".join(arr[:-1])
+        return f"{elements_except_last}, and {arr[-1]}"
+
